b096938aeed96e4435d16e0921a0d91f4d55f28d
galih-bagus <galihbagus555@gmail.com>
2025-02-12 13:33:39 +0700

    add data

---
+++ src/data/module/sub_module/test.data.js
@@ -0,0 +1,11 @@
+export const USER_INVALID = {
+    'username': 'invalid_user',
+    'password': 'standard_user',
+}
+
+export const USER_VALID = {
+    'username': 'standard_user',
+    'password': 'secret_sauce',
+}
+
+export const messageError = `Epic sadface: Username and password do not match any user in this service`
