dcb1d698e8a4f91901d8e3484ba84f19bbb2deca
galih-bagus <galihbagus555@gmail.com>
2025-02-12 13:33:47 +0700

    add page

---
+++ src/page/module/sub_module/test.page.js
@@ -0,0 +1,5 @@
+export const title = `Swag Labs`;
+export const usernameField = `//input[@data-test="username"]`;
+export const passwordField = `//input[@data-test="password"]`;
+export const buttonLogin = `//input[@data-test="login-button"]`;
+export const errorField = `//h3[@data-test="error"]`;
